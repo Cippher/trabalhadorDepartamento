7662f8aa993a29193c02c94edd946b90aef0c72c
VINICIUS DANIEL NEUENFELDT BARROS <viniciusdnbarros@gmail.com>
2019-01-12 18:32:28 -0200

    Atualização da classe App

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -14,11 +14,11 @@ import entidades.emums.NivelDoTrabalhador;
 public class App {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Entre com o nome do departamento: ");
 		String departamento = sc.nextLine();
 		System.out.print("Entre com o nome do trabalhador: ");
@@ -27,35 +27,37 @@ public class App {
 		String nivel = sc.nextLine();
 		System.out.print("Salário base: ");
 		double salario = sc.nextDouble();
-		
-		Trabalhador trabalhador = new Trabalhador(nome, NivelDoTrabalhador.valueOf(nivel), salario, new Departamento(departamento));
-		
+
+		Trabalhador trabalhador = new Trabalhador(nome, NivelDoTrabalhador.valueOf(nivel), salario,
+				new Departamento(departamento));
+
 		System.out.printf("Quantos contratos esse trabalhador possui? ");
 		int numero = sc.nextInt();
-		
-		for(int i = 0; i < numero; i++) {
-			System.out.printf("Entre com a data do #%d contrato: ", i+1);
+
+		for (int i = 0; i < numero; i++) {
+			System.out.printf("Entre com a data do #%d contrato: ", i + 1);
 			System.out.printf("Data DD/MM/AAAA: ");
 			Date data = sdf.parse(sc.next());
 			System.out.printf("Valor por hora: ");
 			double valorPorHora = sc.nextDouble();
 			System.out.printf("Duração: ");
 			int duracao = sc.nextInt();
-			
+
 			HoraContrato contrato = new HoraContrato(data, valorPorHora, duracao);
-			
+
 			trabalhador.addContrato(contrato);
 		}
-		
+
 		System.out.printf("\nEntre com o mes e o ano para calcular o salario: ");
 		String periodo = sc.next();
 		int mes = Integer.parseInt(periodo.substring(0, 2));
 		int ano = Integer.parseInt(periodo.substring(3));
-		
+
 		System.out.println("Nome: " + trabalhador.getNome());
 		System.out.println("Departamento: " + trabalhador.getDepartamento().getNome());
-		System.out.println("Salario no periodo de " + periodo + ": " + String.format("%.2f", trabalhador.salario(ano, mes)));
-		
+		System.out.println(
+				"Salario no periodo de " + periodo + ": " + String.format("%.2f", trabalhador.salario(ano, mes)));
+
 		sc.close();
 	}
 }
